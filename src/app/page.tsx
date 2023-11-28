ed3ced9afed2b0f529bf7943269ac86d00dd77d0
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-11-27 20:46:59 -0500

    Added a footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,16 @@ import LandingContent from "./landingContent";
 
 export default function Home() {
   return (
-
+    <div>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <LandingContent />
     </main>
+
+    <footer className=" flex flex-col items-center justify-center bottom-">
+      <span className="font-default text-sm">
+        © 2023 TicketPro. All rights reserved.
+      </span>
+    </footer>
+    </div>
   )
 }
