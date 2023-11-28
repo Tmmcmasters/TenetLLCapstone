60957dff777de0a28fddb05f8c118f72dc2e9ea7
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-11-27 23:55:45 -0500

    Created the landlord or tenet for login

---
+++ src/app/Login/WhoAreYou/landlordCard.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {Card, CardHeader, CardBody, CardFooter, Button, Image} from "@nextui-org/react"
+
+export default function LandlordCard() {
+    return (
+        <Card className="w-[400px] bg-background/60">
+            <CardBody className="overflow-visible p-0">
+                <Image
+                    alt="Landlord"
+                    src="/landlord.png"
+                    className="w-full object-cover h-[250px]"
+                    width="100%"
+                    height="100%"
+                />
+            </CardBody>
+            <CardFooter className="justify-center">
+                <Button color="primary" size="md" className="w-fill" variant="ghost">
+                    I am a LandLord!
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
---
+++ src/app/SignUp/WhoAreYou/page.tsx
@@ -0,0 +1,5 @@
+export default function WhoAreYou() {
+    return <div>
+        
+    </div>;
+}
