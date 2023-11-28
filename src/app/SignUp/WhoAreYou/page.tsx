cfd1372f4e1a7454808d35b031d428df00914750
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-11-27 23:58:20 -0500

    Added the Who are you to signup

--- src/app/SignUp/WhoAreYou/page.tsx
+++ src/app/SignUp/WhoAreYou/page.tsx
@@ -1,5 +1,17 @@
+import { Spacer } from "@nextui-org/react";
+import LandlordCard from "./landlordCard";
+import TenetCard from "./tenetCard";
+
 export default function WhoAreYou() {
-    return <div>
-        
+    return <div className="flex flex-col items-center justify-center text-center gap-5">
+        <Spacer y={20} />
+        <h1 className="text-2xl  mb-8">
+            Are you a Landlord or a Tenant?
+        </h1>
+        <div className="flex flex-col md:flex-row justify-center   gap-20 md:gap-40">
+        <LandlordCard />
+        <TenetCard />
+        </div>
+        <Spacer y={20} />
     </div>;
 }
