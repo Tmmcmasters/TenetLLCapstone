b7cc8bbed920ff5ce3a1499a9d3de2c00797d656
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-11-30 17:36:42 -0500

    Dashboard Tickets are loading in

--- src/app/Dashboard/Landlord/Components/dashTickets.tsx
+++ src/app/Dashboard/Landlord/Components/dashTickets.tsx
@@ -4,7 +4,7 @@ import DashboardTicketCard from "./dashTicketCard";
 export default function DashboardTickets () {
 
     return (
-        <div className="flex flex-row flex-wrap ml-10">
+        <div className="flex flex-row flex-wrap ml-10 mb-5">
             <DashboardTicketCard/>
         </div>
     )
--- src/app/Dashboard/Landlord/page.tsx
+++ src/app/Dashboard/Landlord/page.tsx
@@ -3,9 +3,15 @@ import NotificationBar from "./Components/notificationBar";
 
 export default function LandlordDashboard() {
     return <div className="flex flex-row items-start justify-start">
-            <NotificationBar/>
-           <div className="flex flex-row items-start justify-start">
-                <DashboardTickets/>
-            </div> 
+        <NotificationBar />
+        <div className="flex flex-col items-start w-full">
+            <h1 className="text-3xl font-semibold self-center  mb-8">Your Tickets</h1>
+            <div className="flex flex-row items-start justify-start flex-wrap">
+                <DashboardTickets />
+                <DashboardTickets />
+                <DashboardTickets />
+                <DashboardTickets />
+            </div>
+        </div>
     </div>
 }
