fa314064aacf51f1555f260b8fae6547b6b65152
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-12-01 16:05:19 -0500

    Fixed some issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='dark h-[100vh]'>
+    <html lang="en" className='dark'>
       <body className={inter.className} >
         <Providers>
           {children}
